ff264712dbd4d5d17dde8d47b7f2dfce79b6ef87
Paul De Ulloa <pauld143@gmail.com>
2023-08-30 15:54:51 -0700

    personCard updated with new content

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -4,9 +4,13 @@ import React from 'react';
 const PersonCard = (props) => {
     return(
         <div>
-            <h1 style={{color:"red"}}>Instructor Bob</h1>
-            <p>Age: 32</p>
-            <p>Hair Color: purple</p>
+            <h1 style={{color:"Blue"}}>Hello Dojo!</h1>
+            <h2>Things I need to do:</h2>
+            <p>* Learn React</p>
+            <p>* Do laundry</p>
+            <p>* Run a to grocery store</p>
+            <p>* Feed the cats</p>
+            
         </div>
     );
 }
